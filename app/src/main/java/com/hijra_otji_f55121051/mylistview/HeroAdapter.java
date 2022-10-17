0b7a9140d3c971e85f003bf9da4eee35be80beb4
hijraotji <hijraotji@gmail.com>
2022-10-18 07:54:32 +0800

    Initial commit

---
+++ app/src/main/java/com/hijra_otji_f55121051/mylistview/HeroAdapter.java
@@ -0,0 +1,65 @@
+package com.hijra_otji_f55121051.mylistview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class HeroAdapter extends BaseAdapter {
+    private final Context context;
+    private ArrayList<Hero> heroes;
+
+    public void setHeroes(ArrayList<Hero> heroes) {
+        this.heroes = heroes;
+    }
+    public HeroAdapter(Context context) {
+        this.context = context;
+        heroes = new ArrayList<>();
+    }
+    @Override
+    public int getCount() {
+        return heroes.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return heroes.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.item_hero, viewGroup, false);
+        }
+        ViewHOlder viewHolder = new ViewHOlder(view);
+        Hero hero = (Hero) getItem(i);
+        viewHolder.bind(hero);
+        return view;
+    }
+    private class ViewHOlder {
+        private TextView textName;
+        private TextView txtDescription;
+        private ImageView imgPhoto;
+
+        ViewHOlder(View view) {
+            textName =view.findViewById(R.id.txt_name);
+            txtDescription = view.findViewById(R.id.txt_description);
+            imgPhoto  = view.findViewById(R.id.img_photo);
+        }
+        void bind(Hero hero) {
+            textName.setText(hero.getName());
+            txtDescription.setText(hero.getDescription());
+            imgPhoto.setImageResource(hero.getPhoto());
+        }
+    }
+}
---
+++ app/src/main/java/com/hijra_otji_f55121051/mylistview/MainActivity.java
@@ -0,0 +1,58 @@
+package com.hijra_otji_f55121051.mylistview;
+
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity{
+    private String[] dataName;
+    private String[] dataDescription;
+    private TypedArray dataPhoto;
+    private HeroAdapter adapter;
+    private ArrayList<Hero> heroes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        adapter = new HeroAdapter(this);
+        ListView listView = findViewById(R.id.lv_list);
+        listView.setAdapter(adapter);
+
+        prepare();
+        addItem();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, heroes.get(i).getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+    private void prepare() {
+        dataName = getResources().getStringArray(R.array.data_name);
+        dataDescription = getResources().getStringArray(R.array.data_description);
+        dataPhoto = getResources().obtainTypedArray(R.array.data_Photo);
+    }
+
+    private void addItem() {
+        heroes = new ArrayList<>();
+
+        for (int i = 0; i < dataName.length; i++) {
+            Hero hero = new Hero();
+            hero.setPhoto(dataPhoto.getResourceId(i, -1));
+            hero.setName(dataName[i]);
+            hero.setDescription(dataDescription[i]);
+            heroes.add(hero);
+        }
+        adapter.setHeroes(heroes);
+    }
+}
